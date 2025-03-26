c3b4021222c30a3dfc04fd3cd5f2a544c9c4e915
Ctbambrose <ctbambrose@gmail.com>
2025-03-25 22:36:38 -0500

    Completed steps for historyService.ts

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -1,4 +1,5 @@
-import fs from 'node:fs/promises'
+import fs from 'node:fs/promises';
+import { v4 as uuidv4 } from 'uuid';
 // TODO: Define a City class with name and id properties
 class City {
   name:string;
@@ -19,13 +20,46 @@ class HistoryService {
     });
   }
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
-  private async write(cities: City[]) {}
+  private async write(cities: City[]) {
+    return await fs.writeFile('db/searchHistory.json', JSON.stringify(cities, null, '\t'))
+  }
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  async getCities() {}
+  async getCities() {
+    return await this.read().then((cities) => {
+      let parsedCities: City[];
+
+      try {
+        parsedCities = [].concat(JSON.parse(cities));
+      } catch (err) {
+        parsedCities = [];
+      }
+      return parsedCities;
+    })
+  }
   // TODO Define an addCity method that adds a city to the searchHistory.json file
-  async addCity(city: string) {}
+  async addCity(city: string) {
+    if (!city) {
+      throw new Error('city cannot be blank');
+    }
+
+    const newCity: City ={name: city, id: uuidv4() };
+
+    return await this.getCities()
+    .then((cities) => {
+      if (cities.find((index) => index.name === city)) {
+        return cities;
+      }
+      return [...cities, newCity];
+    })
+    .then((updateCities) => this.write(updateCities))
+    .then(() => newCity);
+  }
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file
-  async removeCity(id: string) {}
+  async removeCity(id: string) {
+    return await this.getCities()
+    .then ((cities) => cities.filter((city) => city.id !== id))
+    .then ((filteredCities) => this.write(filteredCities));
+  }
 }
 
 export default new HistoryService();
