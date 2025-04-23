20be8db40553a8f4ecaef06ae1fcaced3203d4a6
Ctbambrose <ctbambrose@gmail.com>
2025-04-22 19:35:18 -0500

    updated to mostly working copy

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -1,22 +1,38 @@
 
-import { Router } from 'express';
+import { Router, type Request, type Response } from 'express';
 const router = Router();
 import HistoryService from '../../service/historyService.js';
 import WeatherService from '../../service/weatherService.js';
 // TODO: POST Request with city name to retrieve weather data
-router.post('/:city', (req, res) => {
+router.post('/', async (req: Request, res: Response) => {
+  try {
+    const { city } = req.body;
+    console.log("a string", city)
+    if (!city) {
+        res.status(400).json({ error: 'City name is required' });
+        return
+    }
     // TODO: GET weather data from city name
+    const weatherData = await WeatherService.getWeatherForCity(city);
     // TODO: save city to search history
-    try {
-        const savedCities = HistoryService.getCities();
-        res.json(savedCities);
-      } catch (err) {
-        console.log(err);
-        res.status(500).json(err);
-      }
-});
+    await HistoryService.addCity(city);
+
+    res.json(weatherData);
+  } catch (error) {
+    console.error('Error fetching weather data:', error);
+    res.status(500).json({ error: 'Failed to fetch weather data' });
+  }
+  });
 // TODO: GET search history
-router.get('/history', async (req, res) => { });
+router.get('/history', async (_req: Request, res: Response) => { 
+  try {
+    const savedCities = await HistoryService.getCities();
+    res.json(savedCities);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
+});
 // * BONUS TODO: DELETE city from search history
 router.delete('/history/:id', async (req, res) => { 
     try {
